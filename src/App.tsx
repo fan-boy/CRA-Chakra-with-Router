d99565ade8444930a3c9ca210e067b3dfb03ebb8
Aaditya Shete <aadityashete@gmail.com>
2021-07-31 23:54:47 +0530

    Added routing again

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import * as React from "react"
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import AppRoutes from "./routes/routes"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          
-          
-          
-        </VStack>
-      </Grid>
-    </Box>
+    <AppRoutes />
   </ChakraProvider>
 )
--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -1,9 +1,19 @@
+import { Box, Grid, VStack } from "@chakra-ui/react";
 import React from "react";
+import { ColorModeSwitcher } from "../ColorModeSwitcher";
 
 const Home = () =>{
     return(
-        <>
-        </>
+        <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack spacing={8}>
+          
+          
+          
+        </VStack>
+      </Grid>
+    </Box>
     );
 }
 
